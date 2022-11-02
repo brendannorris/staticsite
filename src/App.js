c35126e4e1d8a93c931a8de7f90ad968394c11f1
brendannorris <brendan.r.norris@gmail.com>
2022-11-02 17:43:38 -0500

    test commit 2

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Suck my tiny nuts
+           Suck my tiny nuts
         </p>
         <a
           className="App-link"
